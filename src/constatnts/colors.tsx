8d943401119c1cb6053cb8108586f010661e0801
developersoncall <developersoncall@gmail.com>
2022-12-12 15:05:57 +0530

    Added custom color

---
+++ src/constatnts/colors.tsx
@@ -0,0 +1,10 @@
+export default{
+    black:'#000',
+    white:'#fff',
+    red:'#f00',
+    text: "#333F48",
+    textSecondary: "#A1AABB",
+    shadowGrey: "#CECECEA2",
+    ocean: "#CDE8E6",
+    oceanSecond: "#E3E3E3",
+}
